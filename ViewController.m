6df9f3d3cacc10b0a6c19e61d95d424f0fb6dcb7
gdsanders <gd@digitalequity.com>
2015-08-31 15:47:51 -0500

    myDog class

--- ViewController.m
+++ ViewController.m
@@ -20,6 +20,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     MBFDog *myDog = [[MBFDog alloc] init];
+    myDog.name = @"Hoss";
     
 }
 

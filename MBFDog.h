8922aa2fc6aa54fa1ac24c12948edbc4014e67ef
gdsanders <gd@digitalequity.com>
2015-08-31 18:04:55 -0500

    MBFDog class

---
+++ MBFDog.h
@@ -0,0 +1,25 @@
+//
+//  MBFDog.h
+//  MBFProj
+//
+//  Created by G.D. Sanders on 8/29/15.
+//  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+@interface MBFDog : NSObject
+
+@property (nonatomic) int age;
+@property (nonatomic, strong) NSString *breed;
+@property (nonatomic, strong) UIImage *image;
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *color;
+
+- (void) bark;
+
+
+
+@end
---
+++ MBFDog.m
@@ -0,0 +1,18 @@
+//
+//  MBFDog.m
+//  MBFProj
+//
+//  Created by G.D. Sanders on 8/29/15.
+//  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+#import "MBFDog.h"
+
+
+@implementation MBFDog
+
+- (void) bark {
+    NSLog(@"Bark bark bark");
+}
+
+@end
